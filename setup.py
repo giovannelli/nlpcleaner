ec623917609f43d51bc1edd2d27205ddeaec82f8
Duccio Giovannelli <giovannelli@extendi.it>
2019-12-16 17:01:28 +0100

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='nlpcleaner',
-      version='0.1.1',
+      version='0.1.2',
       description='Clean and prepare text for modeling with machine learning',
       long_description=long_description,
       long_description_content_type='text/markdown',
